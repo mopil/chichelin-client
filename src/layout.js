f596ca77b85795b5538bc159cd1dc309082a92b7
Harang Kim <berix0218@naver.com>
2022-11-21 17:58:12 +0900

    헤더 제작

---
+++ src/layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './layout.css'
+import Header from './header'
+
+function Layout (){
+
+    return (
+        <div className='layout'>
+            
+        </div>
+    )
+}
+
+export default Layout
