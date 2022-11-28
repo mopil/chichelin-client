28480aafc02ff4a32cecd4f5423acd1897ff3bac
yunseo Kim <0127yuns@naver.com>
2022-11-28 18:42:38 +0900

    검색결과 레이아웃

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React, {useState} from 'react';
 import './App.css';
 import MainPage from './components/mainPage';
@@ -6,15 +5,16 @@ import Layout from './components/layout';
 import Header from './components/header';
 import UploadChicken from './components/uploadChicken';
 import Detailed from './components/detailed';
+import Search from './components/search';
 
 function App() {
 
   return (
     <div>
-      <MainPage></MainPage>
+      {/* <MainPage></MainPage> */}
       {/* <UploadChicken></UploadChicken> */}
       {/* <Detailed></Detailed> */}
-      
+      <Search></Search>
     </div>
   );
 }
