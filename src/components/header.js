f44ccaa1c10d83e2032d3d620832b882c3b4b82f
unknown <eodrmfdl1004@naver.com>
2022-11-28 22:14:32 +0900

    style: 헤더 수정

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,23 @@
 import '../styles/header.css';
 
 function Header() {
-    return (
-        <header className='header'>
-            <h1>Chichelin Guide</h1>
-            <nav>
-                <input type='text' placeholder='브랜드 또는 치킨 입력' />
-                <button type='button' id="img_btn" img src='/images/searchGlass.png'>검색</button>
-            </nav>
-            <div>
-                <img src='/images/ChickenMan.png' width='80' height='80' alt='음...'></img>
-                <img src="/images/speechBubble.png" width='80' height='80' alt="엄"></img>
-            </div>
-        </header>
-    )
+  return (
+    <header className='header'>
+      <h1 className='name'>Chichelin Guide</h1>
+
+      <label className="searchBar">
+        <input className="searchInput" type="search" placeholder="검색"/>
+        <button className='searchButton' type='button'>
+          <img className='searchImage' src='/images/searchGlass.png' alt='search'/>
+        </button>
+      </label>
+
+      <div className='images'>
+        <img src='/images/ChickenMan.png' width='80' height='80' alt='음...'></img>
+        <img src="/images/speechBubble.png" width='80' height='80' alt="엄"></img>
+      </div>
+    </header>
+  )
 }
 
 export default Header
