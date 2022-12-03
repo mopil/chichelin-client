d117d2f29951fc46b5a4d3902f0759a8e23b2d40
unknown <eodrmfdl1004@naver.com>
2022-12-03 14:07:23 +0900

    style: footer 추가

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 function Footer(props) {
   return (
-    <div>
-      footer
-    </div>
+    <footer className='footer'>
+      <h3 className='footerTitle'>팀 프로젝트1</h3>
+      <ul className='members'>
+        <li className='member'>배성흥</li>
+        <li className='member'>팀원1</li>
+        <li className='member'>팀원2</li>
+        <li className='member'>팀원3</li>
+      </ul>
+    </footer>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,26 +15,28 @@ function Header() {
     setSearchWord('');
   };
 
-  return (<header className='header'>
-    <button className='homeButton' onClick={() => movePage('/')}>
-      <h1 className='home'>Chichelin Guide</h1>
-    </button>
-
-    <form className="searchBar" onSubmit={searchChicken}>
-      <input className="searchInput" name="searchWord" value={searchWord} placeholder="검색" onChange={changeValue}/>
-      <button className='searchButton' type='submit'>
-        <img className='searchImage' src='/images/searchGlass.png' alt='search'/>
+  return (
+    <header className='header'>
+      <button className='homeButton' onClick={() => movePage('/')}>
+        <h1 className='home'>Chichelin Guide</h1>
       </button>
-    </form>
 
-    <button className='chickenUploadButton' onClick={() => movePage(`/upload`)}>
-      <img src='/images/ChickenMan.png' height='100%' alt='chickenMan'/>
-      <span className='speechBubble'>
+      <form className="searchBar" onSubmit={searchChicken}>
+        <input className="searchInput" name="searchWord" value={searchWord} placeholder="검색" onChange={changeValue}/>
+        <button className='searchButton' type='submit'>
+          <img className='searchImage' src='/images/searchGlass.png' alt='search'/>
+        </button>
+      </form>
+
+      <button className='chickenUploadButton' onClick={() => movePage(`/upload`)}>
+        <img src='/images/ChickenMan.png' height='100%' alt='chickenMan'/>
+        <span className='speechBubble'>
         <img src="/images/speechBubble.png" height='100%' alt='speechBubble'/>
         <p className='buttonContent'>치킨 <br/> 추가하기</p>
       </span>
-    </button>
-  </header>)
+      </button>
+    </header>
+  )
 }
 
 export default Header
