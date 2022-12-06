77822f6d5e4e7e32b722dd04be77edee5288b8e0
unknown <eodrmfdl1004@naver.com>
2022-12-06 20:22:54 +0900

    style: slider arrow 수정

--- src/components/ChickenSlider.jsx
+++ src/components/ChickenSlider.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import Slider from "react-slick";
+import '../styles/search.css';
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import SliderCard from "./SliderCard";
+
+function NextArrow(props) {
+  const {className, style, onClick} = props;
+  return (
+    <div
+      className={className}
+      style={{...style, display: "block"}}
+      onClick={onClick}
+    />
+  );
+}
+
+function PrevArrow(props) {
+  const {className, style, onClick} = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block" }}
+      onClick={onClick}
+    />
+  );
+}
 
 function ChickenSlider({chickens, row, dots, showLength}) {
   const settings = {
@@ -12,7 +35,9 @@ function ChickenSlider({chickens, row, dots, showLength}) {
     arrows: true,
     dots: dots,
     infinite: true,
-    speed: 800
+    speed: 800,
+    nextArrow: <NextArrow/>,
+    prevArrow: <PrevArrow/>
   };
 
   return (
